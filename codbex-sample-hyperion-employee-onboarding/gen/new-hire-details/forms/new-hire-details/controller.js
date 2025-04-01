ef3062a550760e3f6e1780e610e84b70fdf47179
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-04-01 17:32:42 +0300

    add new hire's details form

---
+++ codbex-sample-hyperion-employee-onboarding/gen/new-hire-details/forms/new-hire-details/controller.js
@@ -0,0 +1,11 @@
+const formView = angular.module('forms', ['ideUI', 'ideView']);
+
+formView.controller('FormController', ($scope, $http, messageHub, ViewParameters) => {
+
+    $scope.forms = {
+        form: {}
+    };
+
+    $scope.model = {};
+    $scope.model. = '';
+});
