4feb887294e73cfcca4fe3a9e79c4a0d5ac7e792
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-04-02 15:13:52 +0300

    add forms for the proccess

---
+++ codbex-sample-hyperion-employee-onboarding/gen/manager-review/forms/manager-review/controller.js
@@ -0,0 +1,13 @@
+const formView = angular.module('forms', ['ideUI', 'ideView']);
+
+formView.controller('FormController', ($scope, $http, messageHub, ViewParameters) => {
+
+    $scope.forms = {
+        form: {}
+    };
+
+    $scope.model = {};
+    $scope.model. = '';
+    $scope.model. = '';
+    $scope.model. = '';
+});
---
+++ codbex-sample-hyperion-employee-onboarding/gen/task-completion/forms/task-completion/controller.js
@@ -0,0 +1,10 @@
+const formView = angular.module('forms', ['ideUI', 'ideView']);
+
+formView.controller('FormController', ($scope, $http, messageHub, ViewParameters) => {
+
+    $scope.forms = {
+        form: {}
+    };
+
+    $scope.model = {};
+});
